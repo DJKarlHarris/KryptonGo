e267ea7eec0050c92245cc14d61813f643b20223
DJKarlHarris <1149338863@qq.com>
2025-05-06 13:19:17 +0000

    feat:zap log

--- main.go
+++ main.go
@@ -4,20 +4,71 @@ import (
 	"KryptonGo/pkg/luban"
 	"KryptonGo/pkg/res"
 	"fmt"
+	"os"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-func main() {
-	var tables *res.Tables
-	var err error
+type App struct {
+	m_logFile     *os.File
+	m_tables      *res.Tables
+	m_logger      *zap.Logger
+	m_sugarLogger *zap.SugaredLogger
+}
 
-	//load table
-	if tables, err = res.NewTables(luban.Loader); err != nil {
+var app App
+
+func exit() {
+	app.m_logger.Sync()
+	app.m_logFile.Close()
+}
+
+func InitLog() (err error) {
+	app.m_logFile, err = os.OpenFile("./KryptonGo.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
 		println(err.Error())
 		return
 	}
 
-	for idx, reward := range tables.TbReward.GetDataList() {
+	fileSyncer := zapcore.AddSync(app.m_logFile)
+	encoder := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+
+	core := zapcore.NewCore(encoder, fileSyncer, zap.DebugLevel)
+
+	app.m_logger = zap.New(core, zap.AddCaller())
+	app.m_sugarLogger = app.m_logger.Sugar()
+
+	return err
+}
+
+func main() {
+	var err error
+
+	//load table
+	if app.m_tables, err = res.NewTables(luban.Loader); err != nil {
+		println(err.Error())
+		return
+	}
+
+	for idx, reward := range app.m_tables.TbReward.GetDataList() {
 		fmt.Printf("idx:%d id:%d name:%s count:%d\n", idx, reward.Id, reward.Name, reward.Count)
 	}
 
+	if err = InitLog(); err != nil {
+		println(err.Error())
+		return
+	}
+
+	var url = "www.4399.com"
+	app.m_sugarLogger.Infow("test",
+		"url", url,
+		"time", 3,
+		"backoff", time.Second,
+	)
+
+	app.m_sugarLogger.Infof("test, url:%s, time:%d, backoff:%s", url, 3, time.Second)
+
+	exit()
 }
